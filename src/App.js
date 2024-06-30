88dcf57ed9a658b317c216ff5f2cc99b97514de6
Jeeva <jeeva.rm@cumulations.com>
2024-06-30 19:26:24 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import Footer from './components/footer/Footer';
+import StoreDetails from './pages/store/StoreDetails';
 
 function App() {
+
+  const queryClient = new QueryClient()
+
+  // Overall layout of the website with header, content and footer
+
+  const Layout = () => {
+
+    return (
+      <div className="app">
+  
+        <QueryClientProvider client={queryClient}>
+          <Navbar />
+          <Outlet />
+          <Footer />
+        </QueryClientProvider>
+  
+      </div>
+    );
+  
+  }
+
+  // URL routes for the website
+  
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/store/:storeId",
+          element: <StoreDetails />
+        }
+  
+      ]
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
+  
 }
 
 export default App;
